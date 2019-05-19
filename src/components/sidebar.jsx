71c3c2d98c4e195cb93365487433b8c29fc7e604
albertmartin_p <am3399150@gmail.com>
2019-05-19 12:14:29 +0700

    first commit

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <a href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></a>
+          <aside id="colorlib-aside" role="complementary" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/3.jpg)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Albert Martin Palmalino</a></h1>
+              <span className="email"><i className="icon-mail"></i> am3399150@gmail.com</span>
+              <span className="number"><i className="icon-phone"></i> +6285648749365</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#" data-nav-section="home">Home</a></li>
+                  <li><a href="#" data-nav-section="about">Profile</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#" data-nav-section="timeline">Portofolio</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li><a href="https://www.facebook.com/albertmartinpalmalino" target="_blank"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://twitter.com/albertmartin_p" target="_blank"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://www.instagram.com/albertmartin_p/" target="_blank"><i className="icon-instagram" /></a></li>
+                <li><a href="https://github.com/albertmartinpalmalino" target="_blank"><i className="icon-github"></i></a></li>
+              </ul>
+            </nav>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
